2dfd9ff2eb72e5404c3fd4fa4f197e25381ad0b8
ark0078 <arkadhar5092@gmail.com>
2024-08-24 20:24:20 +0530

    Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import SignUp from './pages/SignUp'
+
+
+
+import Navbar from './pages/Navbar';
+
+
 
 
 function App() {
 
 
   return (
+    <div>
+         <Navbar/>
+    </div>
+  )
+  return (
     <>
-         <SignUp />
+         <Wellness />
     </>
   )
 }
