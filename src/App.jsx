178c9b5c7995738d4494bf7d858b0a3ba5c10e69
ark0078 <arkadhar5092@gmail.com>
2024-08-25 02:01:21 +0530

    made wellness

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 
 // import Wellness from "./pages/Wellness"
-import Navbar from './pages/Wellness'
+import Wellness from './pages/Wellness'
 import { useState } from 'react'
 import './App.css'
 // import SignIn from './pages/SignIn'
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div>
-         {/* <Navbar/> */}
+         <Wellness/>
     </div>
   )
 }
--- src/pages/Wellness.jsx
+++ src/pages/Wellness.jsx
@@ -5,17 +5,20 @@ import Footer from '../components/Footer.jsx'
 const Wellness = () => {
   return (
     <>
-<Navbar/>
-    <div className='flex flex-wrap bg-white w-[50%] '>
+<Navbar menu />
+<div className="outer-cont w-[100vw] h-[calc(100vh-70px)] flex justify-center ">
+    <div className='flex flex-col bg-white w-[50%] '>
+      <div className="inn flex flex-wrap h-[calc(100%-60px)] ">
       <Wellnessbox path="water.svg"/> 
       <Wellnessbox path="meditation.svg"/> 
       <Wellnessbox path="food.svg"/> 
       <Wellnessbox path="game.svg"/> 
       <Wellnessbox path="chat1.svg"/> 
-       </div>
+      </div>
        <Footer/>
+       </div>
        
-       
+       </div>
     </>
 
 
