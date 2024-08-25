1de8394cb0b6c01b18b69fef5e38144b24253271
GLXALOKESH <alokeshmaitra9@gmail.com>
2024-08-25 11:02:50 +0530

    routes reaact

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 // import { useState } from 'react'
 import 'leaflet/dist/leaflet.css';
 // import Wellness from "./pages/Wellness"
-import Wellness from './pages/Wellness'
-import { useState } from 'react'
+// import Wellness from './pages/Wellness'
+// import { useState } from 'react'
 import './App.css'
-import Map from './pages/Map.jsx';
+// import Map from './pages/Map.jsx';
 // import SignIn from './pages/SignIn'
 // import Signup from './pages/SignUp'
+import Homepage from "./pages/Homepage.jsx"
+// import Landing from './pages/Landing.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Landing  from './pages/Landing.jsx';
+import Map from "./pages/Map.jsx"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/maps",
+    element: <Map />,
+  },
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
 
 
   return (
-    <div>
-         <Map/>
-    </div>
+    <>
+          <RouterProvider router={router} />
+    </                                                                                                                                                                                                                                                                        >
   )
 }
 
